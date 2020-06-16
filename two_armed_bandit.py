d5a59ad3f9f22c7e896739e347d9e01c8b07fcb4
Lena Esther Ptasczynski <66962092+eptas@users.noreply.github.com>
2020-06-16 14:14:10 +0200

    Add files via upload

---
+++ two_armed_bandit.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class TwoArmedBandit:
+
+    def __init__(self, nbandits=5, values=None, sd=1):
+
+        self.nbandits = nbandits
+        self.values = np.arange(nbandits) if values is None else np.array(values)
+        self.sd = sd
+
+    def get_stimuli(self):
+        """This function outputs two random stimulus indices."""
+
+        return np.random.randint(0, self.nbandits, 2)
+
+    def get_outcome(self, choice):
+        """This function outputs a reward value matrix for random stimulus presentation."""
+
+        return np.random.normal(loc=self.values[choice], scale=self.sd)

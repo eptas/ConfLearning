916a833c353d7627a62a8ce06713a8dee305e344
m-guggenmos <monaden@gmail.com>
2021-06-02 14:47:32 +0200

    Add new plot for AIC; suggestion how to import local packages

--- plot/plot_aic_mg.py
+++ plot/plot_aic_mg.py
@@ -18,8 +18,12 @@ color_aic = (0.6, 0.6, 0.6)
 
 # model_name = ['Rescorla\nStatic', 'Rescorla\nZero', 'Rescorla\nConf', 'Rescorla\nConfGen', 'Rescorla\nConfNofeed', 'Rescorla\nConfNofeedGen',
 #               'Rescorla\nConfZero', 'Rescorla\nConfZeroGen', 'Rescorla\nConfNofeedZero', 'Rescorla\nConfNofeedZeroGen']
-model_name = ['Static', 'Zero', 'Spec+Dual', 'Unspec+Dual', 'Spec+Mono', 'Unspec+Mono',
-              'Zero+Spec+Dual', 'Zero+Unspec+Dual', 'Zero+Spec+Mono', 'Zero+Unspec+Mono']
+model_name = ['NoDeval [-]', 'Deval [-]', 'NoDeval [Spec+Dual]', 'NoDeval [Unspec+Dual]',
+              'NoDeval [Spec+Mono]', 'NoDeval [Unspec+Mono]', 'Deval [Spec+Dual]', 'Deval [Unspec+Dual]',
+              'Deval [Spec+Mono]', 'Deval [unspec+Mono]']
+model_name = ['[-]', '[-]$^\mathrm{d}$', '[dual+spec]', '[dual+unspec]',
+              '[mono+spec]', '[mono+unspec]', '[dual+spec]$^\mathrm{d}$', '[dual+unspec]$^\mathrm{d}$',
+              '[mono+spec]$^\mathrm{d}$', '[mono+unspec]$^\mathrm{d}$']
 n_models = len(model_name)
 n_subjects = 66
 
@@ -39,12 +43,12 @@ plt.gca().xaxis.grid('silver', linestyle='-', linewidth=0.4, zorder=-5)
 plt.barh(range(n_models), AICm[order], xerr=AICe, color=color_aic, zorder=2)
 
 for t in range(n_models):
-    plt.text(407.2, order[t]-0.2, model_name[order[t]], color='k', fontsize=10, zorder=3)
+    plt.text(400.4, t-0.2, model_name[order[t]], color='k', fontsize=10, zorder=3)
 
 plt.xlabel('AIC')
-# plt.xticks(np.arange(n_models), model_name, fontsize=10)
+plt.xticks(range(400, 426, 5))
 plt.yticks(range(n_models), [])
-plt.xlim(407, 426)
+plt.xlim(400, 425.5)
 plt.ylim(-0.6, n_models-0.4)
 set_fontsize(xlabel=12, tick=10)
 savefig('../figures/fitting/AIC_ConfSim_MG.png')
